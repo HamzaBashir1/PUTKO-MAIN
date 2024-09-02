234c2556643137ca12942626d7e797a11583f5d6
Hamza <Hamzabashir02001@gmail.com>
2024-09-02 13:18:56 +0500

    first

---
+++ frontend/src/app/PropertyDetail/page.js
@@ -0,0 +1,34 @@
+"use client";
+import { useState, useEffect } from 'react';
+import React from 'react';
+import Heading from './component/Heading';
+
+import Date from './component/Date';
+import Photo from './component/Photo';
+import Information from './component/Information'
+import ReservationCard from './component/ReservationCard'
+
+const Page = () => {
+  
+
+  return (
+    <div className='bg-[#f8f8f8]'>
+      <Heading />
+      <Photo />
+      <ReservationCard/>
+      <Date />
+    <Information/>
+      
+      {/* <div className='flex flex-row'>
+        <div className='flex-1'>
+            
+        </div>
+        <div className='mr-96'>
+            
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default Page;
