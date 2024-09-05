1e7fd4b612ca606873fc6130c1090a68041197e5
Hamza <Hamzabashir02001@gmail.com>
2024-09-05 20:04:46 +0500

    fix api error

--- frontend/src/app/login/component/login.js
+++ frontend/src/app/login/component/login.js
@@ -59,10 +59,10 @@ const Login = () => {
   };
 
   return (
-    <section className="px-5 lg:px-[700px] w">
-      <div className="w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+    <section className="px-5 lg:px-[700px] ">
+      <div className="lg:w-[570px] lg:mx-auto rounded-lg shadow-md md:p-10">
         <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
-          Hello! <span className="text-primaryColor">Welcome</span> Back
+          Hello! <span className="text-[#4FBE9F]">Welcome</span> Back
         </h3>
 
         <form className="py-4 md:py-0" onSubmit={submitHandler}>
